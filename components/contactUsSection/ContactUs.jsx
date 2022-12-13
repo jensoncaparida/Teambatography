3f30907d5209a14068da5e66b81a89f682169cbc
Jenson Caparida <jensoncaparida@gmail.com>
2022-12-13 20:27:27 +0800

    update

--- components/contactUsSection/ContactUs.jsx
+++ components/contactUsSection/ContactUs.jsx
@@ -13,11 +13,10 @@ export default function ContactUs() {
   } = useForm();
 
   const onSubmit = async (data) => {
-    const config = fetch("/api/contact", {
+    const config = fetch("api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json",
       },
 
       body: JSON.stringify(data),
